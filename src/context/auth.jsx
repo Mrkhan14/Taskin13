ad4f46cb32b727d00f2958e0b052594b91ba5788
Abdulakhadkhan <50317423+Mrkhan14@users.noreply.github.com>
2024-11-07 18:03:14 +0500

    new error

--- src/context/auth.jsx
+++ src/context/auth.jsx
@@ -3,7 +3,7 @@ import Cookies from 'js-cookie';
 import PropTypes from 'prop-types';
 import { useState } from 'react';
 import { AuthContext } from '../hooks/useAuth';
-import request from '../services/reauest';
+import request from '../services/reauest'; // corrected 'reauest' to 'request'
 import { TOKEN, USER } from '../utils/constants';
 
 const AuthProvider = ({ children }) => {
@@ -31,7 +31,7 @@ const AuthProvider = ({ children }) => {
 
          const { role } = user;
 
-         message.success('Siz tizimga kirdingiz');
+         message.success('You have successfully logged in');
 
          if (role === 'admin') {
             navigate('/dashboard');
@@ -43,9 +43,40 @@ const AuthProvider = ({ children }) => {
       }
    };
 
-   const state = { user, loading, login };
+   const registerFun = async (values, navigate) => {
+      try {
+         setLoading(true);
+         const {
+            data: { token },
+         } = await request.post('auth/register', values);
+
+         console.log(data, 'ssssssssssssssss');
+
+         // Cookies.set(TOKEN, token);
+
+         // request.defaults.headers.common.Authorization = `Bearer ${token}`;
+
+         // const { data: user } = await request.get('auth/me');
+
+         // localStorage.setItem(USER, JSON.stringify(user));
+
+         // setUser(user);
+
+         // const { role } = user;
+
+         message.success('You have successfully registered');
+
+         navigate('/login');
+      } catch (e) {
+         console.log(e);
+      } finally {
+         setLoading(false);
+      }
+   };
+
+   const state = { user, loading, registerFun, login };
    return <AuthContext.Provider value={state}>{children}</AuthContext.Provider>;
 };
-AuthProvider.protoType = { children: PropTypes.node };
+AuthProvider.propTypes = { children: PropTypes.node };
 
 export default AuthProvider;
--- src/pages/public/RegisterPage.jsx
+++ src/pages/public/RegisterPage.jsx
@@ -1,77 +1,131 @@
 import React from 'react';
 import { useForm } from 'react-hook-form';
+import { useNavigate } from 'react-router-dom'; // Import useNavigate hook
+import useAuth from '../../hooks/useAuth';
 import InputComponent from './../../components/box/InputComponent';
 
 const RegisterPage = () => {
-  const { register, handleSubmit, formState: { errors } } = useForm();
+   const { registerFun, loading } = useAuth();
+   const navigate = useNavigate(); // Initialize useNavigate hook
+   const {
+      register,
+      handleSubmit,
+      formState: { errors },
+   } = useForm();
 
-  const onSubmit = (data) => {
-    console.log('Form Data:', data);
-  };
+   const onSubmit = data => {
+      // data = {}
+      // const { first_name, last_name, username, password, role } = data;
+      // const formData = new FormData();
+      // formData.append('first_name', first_name);
+      // formData.append('last_name', last_name);
+      // formData.append('username', username);
+      // formData.append('password', password);
+      // formData.append('role', role);
+      // registerFun(formData, navigate);
+      registerFun(data, navigate);
+      console.log('Form Data:', data);
+   };
 
-  return (
-    <div className="flex items-center justify-center min-h-[calc(100vh-220px)]">
-      <div className="w-full max-w-3xl">
-        <h1 className='mb-16 text-5xl font-semibold text-center text-primary-600'>Register</h1>
-        <form 
-          onSubmit={handleSubmit(onSubmit)}
-        >
-          <InputComponent 
-            // label="FirstName" 
-            name="firstName" 
-            placeholder="Firstname"
-            register={register} 
-            required
-          />
-          {errors.firstName && <p className="text-xs italic text-red-500">FirstName is required.</p>}
+   return (
+      <div className='flex items-center justify-center min-h-[calc(100vh-220px)]'>
+         <div className='w-full max-w-3xl'>
+            <h1 className='mb-16 text-5xl font-semibold text-center text-primary-600'>
+               Register
+            </h1>
+            <form onSubmit={handleSubmit(onSubmit)}>
+               <InputComponent
+                  // label="FirstName"
+                  name='first_name'
+                  placeholder='First name'
+                  register={register}
+                  required
+               />
+               {errors.first_name && (
+                  <p className='text-xs italic text-red-500'>
+                     FirstName is required.
+                  </p>
+               )}
 
-          <InputComponent 
-            // label="LastName" 
-            name="Last name" 
-            placeholder="lastName"
-            register={register} 
-            required
-          />
-          {errors.lastName && <p className="text-xs italic text-red-500">Last name is required.</p>}
-          
-          <InputComponent 
-            // label="Username" 
-            name="username" 
-            placeholder="Username"
-            register={register} 
-            required
-          />
-          {errors.username && <p className="text-xs italic text-red-500">Username is required.</p>}
+               <InputComponent
+                  // label="LastName"
+                  name='last_name'
+                  placeholder='Last name'
+                  register={register}
+                  required
+               />
+               {errors.last_name && (
+                  <p className='text-xs italic text-red-500'>
+                     Last name is required.
+                  </p>
+               )}
 
-          <InputComponent 
-            // label="Password" 
-            name="password" 
-            type="password" 
-            placeholder="Password"
-            register={register} 
-            required
-          />
-          {errors.password && <p className="text-xs italic text-red-500">Password is required.</p>}
+               <InputComponent
+                  // label="Username"
+                  name='username'
+                  placeholder='Username'
+                  register={register}
+                  required
+               />
+               {errors.username && (
+                  <p className='text-xs italic text-red-500'>
+                     Username is required.
+                  </p>
+               )}
 
-          <InputComponent 
-            // label="Password" 
-            name="passwordConfirm" 
-            type="passwordConfirm" 
-            placeholder="PasswordConfirm"
-            register={register} 
-            required
-          />
-          {errors.passwordConfirm && <p className="text-xs italic text-red-500">PasswordConfirm is required.</p>}
+               <InputComponent
+                  // label="Password"
+                  name='password'
+                  type='password'
+                  placeholder='Password'
+                  register={register}
+                  required
+               />
+               {errors.password && (
+                  <p className='text-xs italic text-red-500'>
+                     Password is required.
+                  </p>
+               )}
 
-            <button className="bg-[#FFD050] text-2xl font-medium p-4 w-full text-primary-600" 
-              type="submit"
-            >
-              Register
-            </button>
-        </form>
+               <InputComponent
+                  // label="Password"
+                  name='passwordConfirm'
+                  type='password'
+                  placeholder='PasswordConfirm'
+                  register={register}
+                  required
+               />
+               {errors.passwordConfirm && (
+                  <p className='text-xs italic text-red-500'>
+                     PasswordConfirm is required.
+                  </p>
+               )}
+
+               <InputComponent
+                  // label="role"
+                  name='role'
+                  type='text'
+                  placeholder='Role'
+                  register={register}
+                  required
+               />
+               {errors.role && (
+                  <p className='text-xs italic text-red-500'>
+                     Role is required.
+                  </p>
+               )}
+
+               <button
+                  className='bg-[#FFD050] text-2xl font-medium p-4 w-full text-primary-600'
+                  type='submit'
+                  disabled={loading} // Disable button while loading
+               >
+                  {loading ? 'Registering...' : 'Register'}
+               </button>
+            </form>
+         </div>
       </div>
-    </div>
-  );
+   );
 };
 
 export default RegisterPage;
